4a5ec72a7773b1bdda020400f427d03b01d17a77
touseefurehman <tousifurehman2084@gmail.com>
2024-11-22 08:07:44 +0500

    req

--- began/settings.py
+++ began/settings.py
@@ -61,7 +61,7 @@ TEMPLATES = [
 WSGI_APPLICATION = 'began.wsgi.application'
 
 
-# Database
+# Database          hbkj
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
